6c65e2329173caa85b6bf296b55ee53e0d2dabe2
xvferdy <xv.ferdy@gmail.com>
2021-11-14 10:42:47 +0800

    ♻️ Branch deploy
    contextAPI

--- src/App.js
+++ src/App.js
@@ -1,53 +1,51 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-
 import questionsDb from "./helpers/questionsDb";
 import moneyPyramid from "./helpers/moneyPyramid";
 import RightPanel from "./components/RightPanel";
 import LeftPanel from "./components/LeftPanel";
-
 import { GameStateContext } from "./helpers/Context";
 
 function App() {
-  const [questionNumber, setQuestionNumber] = useState(1);
-  const [stop, setStop] = useState(false);
-  const [earned, setEarned] = useState("Rp. 0");
+	const [questionNumber, setQuestionNumber] = useState(1);
+	const [stop, setStop] = useState(false);
+	const [earned, setEarned] = useState("Rp. 0");
 
-  useEffect(() => {
-    if (questionNumber > 1) {
-      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
-    }
-  }, [questionNumber]);
+	useEffect(() => {
+		if (questionNumber > 1) {
+			setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+		}
+	}, [questionNumber]);
 
-  const handelePlayAgain = () => {
-    setStop(false);
-    setQuestionNumber(1);
-    setEarned("$ 0");
-  };
+	const handelePlayAgain = () => {
+		setStop(false);
+		setQuestionNumber(1);
+		setEarned("$ 0");
+	};
 
-  return (
-    <div className="app">
-      <GameStateContext.Provider
-        value={{
-          earned,
-          stop,
-          handelePlayAgain,
-          questionsDb,
-          questionNumber,
-          setQuestionNumber,
-          setStop,
-          setEarned,
-          moneyPyramid,
-        }}
-      >
-        {/* MAIN */}
-        <LeftPanel />
+	return (
+		<div className="app">
+			<GameStateContext.Provider
+				value={{
+					earned,
+					stop,
+					handelePlayAgain,
+					questionsDb,
+					questionNumber,
+					setQuestionNumber,
+					setStop,
+					setEarned,
+					moneyPyramid,
+				}}
+			>
+				{/* MAIN */}
+				<LeftPanel />
 
-        {/* PYRAMID */}
-        <RightPanel questionNumber={questionNumber} />
-      </GameStateContext.Provider>
-    </div>
-  );
+				{/* PYRAMID */}
+				<RightPanel questionNumber={questionNumber} />
+			</GameStateContext.Provider>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -2,65 +2,65 @@ import React, { useState, useEffect, useContext } from "react";
 import { GameStateContext } from "../helpers/Context";
 
 function Trivia() {
-  const {
-    questionsDb,
-    questionNumber,
-    setQuestionNumber,
-    setStop,
-    setEarned,
-    moneyPyramid,
-  } = useContext(GameStateContext);
+	const {
+		questionsDb,
+		questionNumber,
+		setQuestionNumber,
+		setStop,
+		setEarned,
+		moneyPyramid,
+	} = useContext(GameStateContext);
 
-  const [question, setQuestion] = useState(null);
-  const [className, setClassName] = useState("answer");
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
+	const [question, setQuestion] = useState(null);
+	const [className, setClassName] = useState("answer");
+	const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  useEffect(() => {
-    setQuestion(questionsDb[questionNumber - 1]);
-  }, [questionsDb, questionNumber]);
+	useEffect(() => {
+		setQuestion(questionsDb[questionNumber - 1]);
+	}, [questionsDb, questionNumber]);
 
-  const handleClick = (a) => {
-    setSelectedAnswer(a);
-    setClassName("answer active");
+	const handleClick = (a) => {
+		setSelectedAnswer(a);
+		setClassName("answer active");
 
-    setTimeout(() => {
-      setClassName(a.correct ? "answer correct" : "answer wrong");
-    }, 200);
+		setTimeout(() => {
+			setClassName(a.correct ? "answer correct" : "answer wrong");
+		}, 200);
 
-    setTimeout(() => {
-      if (a.correct && questionNumber < questionsDb.length) {
-        // setQuestionNumber((prev) => prev + 1); //multiple click = setting up multiple state
-        setQuestionNumber((prev) => questionNumber + 1);
-        setSelectedAnswer(null);
-      }
-      //soal terakhir
-      else if (a.correct) {
-        setEarned(`${moneyPyramid[0].amount} !!!!!`);
-        setSelectedAnswer(null);
-        setStop(true);
-      } else {
-        setStop(true);
-      }
-    }, 1500);
-  };
+		setTimeout(() => {
+			if (a.correct && questionNumber < questionsDb.length) {
+				// setQuestionNumber((prev) => prev + 1); //multiple click = setting up multiple state
+				setQuestionNumber((prev) => questionNumber + 1);
+				setSelectedAnswer(null);
+			}
+			// soal terakhir
+			else if (a.correct) {
+				setEarned(`${moneyPyramid[0].amount} !!!!!`);
+				setSelectedAnswer(null);
+				setStop(true);
+			} else {
+				setStop(true);
+			}
+		}, 1500);
+	};
 
-  return (
-    <div className="trivia">
-      <div className="question">{question?.question}</div>
-      <div className="answers">
-        {question?.answers.map((answer) => (
-          <div
-            className={selectedAnswer === answer ? className : "answer"}
-            onClick={(e) => {
-              handleClick(answer);
-            }}
-          >
-            {answer.text}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="trivia">
+			<div className="question">{question?.question}</div>
+			<div className="answers">
+				{question?.answers.map((answer) => (
+					<div
+						className={selectedAnswer === answer ? className : "answer"}
+						onClick={(e) => {
+							handleClick(answer);
+						}}
+					>
+						{answer.text}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Trivia;
